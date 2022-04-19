994483b5d13cd68e2e9756b9617998fd385b9e35
m-arc <m-arc@192.168.1.8>
2022-04-19 09:37:35 -0300

    Commit desafio 116

---
+++ src/classe/Comida.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class Comida {
+
+	String nome;
+	double peso;
+
+	Comida(String comida, Double peso) {
+		this.nome = comida;
+		this.peso = peso;
+	}
+}
---
+++ src/classe/Pessoa.java
@@ -0,0 +1,23 @@
+package classe;
+
+public class Pessoa {
+
+	String nome;
+	double peso;
+
+	Pessoa(String nome, double peso) {
+		this.nome = nome;
+		this.peso = peso;
+	}
+
+	void comer(Comida comida) {
+		if(comida != null) {
+			this.peso += comida.peso;
+		}
+	}
+	
+	String apresentar() {
+		return "Olá eu sou o" + this.nome + " e tenho " + this.peso + " Kg.";
+	}
+
+}
